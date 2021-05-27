6bd694f1503d511f2718c3737e9d1b0408c9da19
igoreta <ivoronline@gmail.com>
2021-05-27 13:43:39 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/mavenjaxb2plugin_generateclasses_fromxsd_local/Mavenjaxb2pluginGenerateclassesFromxsdLocalApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.mavenjaxb2plugin_generateclasses_fromxsd_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavenjaxb2pluginGenerateclassesFromxsdLocalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Mavenjaxb2pluginGenerateclassesFromxsdLocalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/mavenjaxb2plugin_generateclasses_fromxsd_local/Mavenjaxb2pluginGenerateclassesFromxsdLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.mavenjaxb2plugin_generateclasses_fromxsd_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavenjaxb2pluginGenerateclassesFromxsdLocalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
